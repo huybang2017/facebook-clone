fd67f591b47c0c63314727f42651fbf93e8df917
huybang2017 <ndhbang2017@gmail.com>
2025-04-06 01:34:40 +0700

    chore: update logic service with cloudinary

--- server/src/main/java/com/facebook/server/service/ImageService.java
+++ server/src/main/java/com/facebook/server/service/ImageService.java
@@ -1,13 +1,16 @@
 package com.facebook.server.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.facebook.server.entity.Image;
 import com.facebook.server.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -15,6 +18,9 @@ public class ImageService {
 
     @Autowired
     private ImageRepository imageRepository;
+
+    @Autowired
+    private Cloudinary cloudinary;
 
     public List<Image> getAllImages() {
         return imageRepository.findAll();
@@ -24,16 +30,23 @@ public class ImageService {
         return imageRepository.findById(id);
     }
 
-    public Image createImage(Image image) {
+    public Image uploadImage(MultipartFile file) throws IOException {
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        String imageUrl = (String) uploadResult.get("secure_url");
+
+        Image image = new Image();
+        image.setImage(imageUrl);
         return imageRepository.save(image);
     }
 
-    public Image updateImage(String id, Image imageDetails) {
+    public Image updateImage(String id, MultipartFile file) throws IOException {
         Optional<Image> optionalImage = imageRepository.findById(id);
         if (optionalImage.isPresent()) {
+            Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+            String imageUrl = (String) uploadResult.get("secure_url");
+
             Image image = optionalImage.get();
-            image.setImage(imageDetails.getImage());
-            image.setUpdatedAt(LocalDateTime.now());
+            image.setImage(imageUrl);
             return imageRepository.save(image);
         } else {
             throw new RuntimeException("Image not found with id " + id);
