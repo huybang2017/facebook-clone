e4cc53e094556bee5dcfb1290f0f491b33771b93
huybang2017 <ndhbang2017@gmail.com>
2025-04-06 01:31:52 +0700

    chore: update logic controller with cloudinary

--- server/src/main/java/com/facebook/server/controller/ImageController.java
+++ server/src/main/java/com/facebook/server/controller/ImageController.java
@@ -1,17 +1,18 @@
 package com.facebook.server.controller;
 
+import com.facebook.server.entity.Image;
+import com.facebook.server.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.facebook.server.entity.Image;
-import com.facebook.server.service.ImageService;
-
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/images")
 public class ImageController {
 
     @Autowired
@@ -25,23 +26,27 @@ public class ImageController {
     @GetMapping("/{id}")
     public ResponseEntity<Image> getImageById(@PathVariable String id) {
         Optional<Image> image = imageService.getImageById(id);
-        if (image.isPresent()) {
-            return ResponseEntity.ok(image.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return image.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Image createImage(@RequestBody Image image) {
-        return imageService.createImage(image);
+    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            Image image = imageService.uploadImage(file);
+            return ResponseEntity.ok(image);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Image> updateImage(@PathVariable String id, @RequestBody Image imageDetails) {
+    public ResponseEntity<Image> updateImage(@PathVariable String id, @RequestParam("file") MultipartFile file) {
         try {
-            Image updatedImage = imageService.updateImage(id, imageDetails);
+            Image updatedImage = imageService.updateImage(id, file);
             return ResponseEntity.ok(updatedImage);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
