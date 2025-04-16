7dcb464873d9a9661899cb0c8fe3f6d0d2f0b09f
phucdev66 <phucledev66@gmail.com>
2025-04-17 02:06:15 +0700

    Xác thực quyền truy cập vào các route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
 import { Route, Routes } from "react-router-dom";
-import { privateRoute } from "./routes";
+import { privateRoute, publicRoute } from "./routes";
 import DefaultLayout from "./pages/layouts/DefaultLayout";
 import { Fragment } from "react";
 import StoreProvider from "./contexts/StoreProvider";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import GuestRoute from "./routes/GuestRoute";
 
 const App = () => {
   return (
     <StoreProvider>
       <Routes>
+        {/* Đây là các route không cần phải đăng nhập mà vẫn có quyền truy cập */}
+        {publicRoute.map((route, index) => {
+          const Page = route.element;
+          let Layout = route.layout ?? DefaultLayout;
+          if (route.layout === null) Layout = Fragment;
+
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <GuestRoute>
+                  <Layout>
+                    <Page />
+                  </Layout>
+                </GuestRoute>
+              }
+            />
+          );
+        })}
+
+        {/* Đây là các route cần phải đăng nhập/xác thực thì mới có quyền truy cập */}
         {privateRoute.map((route, index) => {
           const Page = route.element;
 
@@ -24,9 +48,11 @@ const App = () => {
               key={index}
               path={route.path}
               element={
-                <Layout>
-                  <Page />
-                </Layout>
+                <ProtectedRoute>
+                  <Layout>
+                    <Page />
+                  </Layout>
+                </ProtectedRoute>
               }
             />
           );
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -8,17 +8,21 @@ import OnlyHeader from "@/pages/layouts/OnlyHeader";
 import NotFound from "@/pages/exception/NotFound";
 import CommingSoon from "@/pages/exception/CommingSoon";
 import HeaderSidebar from "@/pages/layouts/HeaderSidebar";
+
+const publicRoute = [
+  { path: "/login", element: Login, layout: null },
+  { path: "/register", element: Register, layout: null },
+];
+
 const privateRoute = [
   { path: "/", element: Home },
   { path: "/create-post", element: CreatePost, layout: OnlyHeader },
   { path: "/profile", element: Profile, layout: OnlyHeader },
   { path: "/friends", element: Friends, layout: HeaderSidebar },
-  { path: "/login", element: Login, layout: null },
-  { path: "/register", element: Register, layout: null },
   { path: "/video", element: CommingSoon, layout: null },
   { path: "/market", element: CommingSoon, layout: null },
   { path: "/game", element: CommingSoon, layout: null },
   { path: "/*", element: NotFound, layout: null },
 ];
 
-export { privateRoute };
+export { privateRoute, publicRoute };
